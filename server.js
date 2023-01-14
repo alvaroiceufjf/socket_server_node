c05f150da576e4c12402996ade77bf0d3ea2ffb0
alvaro <alvaro.domingues@estudante.ufjf.br>
2023-01-13 21:49:05 -0300

    adiciona chamados de emergencia e bateria baixa

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ console.log("server is running");
 
 io.on("connection", (socket) => {
   // send a message to the client
+  let emergencyMode = false;
+  let batteryLow = false;
+  if (batteryLow){
+    socket.emit("batteryAlert", false);
+  }else if(emergencyMode){
+    socket.emit("emergency", true);
+  }else{
+    socket.emit("hello from server", true);
+  };
+  
+  
 
-  socket.emit("hello from server", true);
+
 
   /*
   // receive a message from the client
